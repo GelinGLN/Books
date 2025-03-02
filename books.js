54b04a401f7efbc47ea619da03376c641004cdf9
João Pedro <jaumpredo8068@gmail.com>
2025-03-02 13:30:04 -0300

    v0.1.0: Estrutura base do módulo books

---
+++ books.js
@@ -0,0 +1,9 @@
+const library = require('./library.json');
+class Book {
+    static execute(UserInput) {
+        if (UserInput === '/book') {
+            // Aqui vem o código de execução do comando /book
+        }
+    }
+}
+module.exports = Book;
